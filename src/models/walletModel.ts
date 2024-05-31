754600c0b9bc235f2c5d9d21a3a78ddd5191629a
Desmondflexy <ikilodesmond@gmail.com>
2024-05-31 09:31:59 +0100

    add wallet model

---
+++ src/models/walletModel.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, Types, model } from "mongoose";
+
+export interface WalletDocument extends Document {
+    customerId: Types.ObjectId;
+    balance: number;
+    transactions: {
+        type: 'debit | credit';
+        amount: number;
+        date: Date;
+    }[];
+}
+
+const walletSchema = new Schema<WalletDocument>({
+    customerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    balance: { type: Number, default: 0.00 },
+    transactions: [
+        {
+            type: { type: String, enum: ["debit", "credit"], required: true },
+            amount: { type: Number, required: true },
+            date: { type: Date, default: Date.now }
+        }
+    ]
+}, {
+    timestamps: true
+});
+
+export default model<WalletDocument>("Wallet", walletSchema);
