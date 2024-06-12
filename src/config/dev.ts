1d3176c9792d4d106285d87370457ad6555b3ae0
Desmondflexy <ikilodesmond@gmail.com>
2024-06-12 12:42:46 +0100

    codebase restructure

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const { DEV_PORT, MORGAN, DEV_MONGO_URI } = process.env;
+// const { DEV_PORT, MORGAN, DEV_MONGO_URI } = process.env;
 
-export default {
-  port: DEV_PORT,
-  logs: {
-    morgan: MORGAN!,
-  },
-  mongoURI: DEV_MONGO_URI!,
-};
+// export default {
+//   port: DEV_PORT,
+//   logs: {
+//     morgan: MORGAN!,
+//   },
+//   mongoURI: DEV_MONGO_URI!,
+// };
--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const { PROD_PORT, MORGAN, PROD_MONGO_URI } = process.env;
+// const { PROD_PORT, MORGAN, PROD_MONGO_URI } = process.env;
 
-export default {
-  port: PROD_PORT,
-  logs: {
-    morgan: MORGAN!,
-  },
-  mongoURI: PROD_MONGO_URI!,
-};
+// export default {
+//   port: PROD_PORT,
+//   logs: {
+//     morgan: MORGAN!,
+//   },
+//   mongoURI: PROD_MONGO_URI!,
+// };
