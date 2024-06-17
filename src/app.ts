3f2294d163647ff66212e37b1a1117c585d169ae
guudc <65718626+guudc@users.noreply.github.com>
2024-06-17 15:06:10 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -40,36 +40,36 @@ app.listen(PORT, () => {
 })
 
 
-import config from "./config/index";
-import userRoutes from "./routes/userRoutes";
-import orderRoutes from "./routes/orderRoutes";
-import txRoutes from "./routes/txRoutes";
-import databaseConnection from "./loaders/mongodbLoader";
-import loadingExpressApp from "./loaders/indexLoader";
-import riderRoutes from "./routes/riderRoute"
+// import config from "./config/index";
+// import userRoutes from "./routes/userRoutes";
+// import orderRoutes from "./routes/orderRoutes";
+// import txRoutes from "./routes/txRoutes";
+// import databaseConnection from "./loaders/mongodbLoader";
+// import loadingExpressApp from "./loaders/indexLoader";
+// import riderRoutes from "./routes/riderRoute"
 
-async function startServer() {
-  const app = express();
+// async function startServer() {
+//   const app = express();
 
-  const { port } = config;
+//   const { port } = config;
 
-  await databaseConnection({});
-  await loadingExpressApp({ app });
+//   await databaseConnection({});
+//   await loadingExpressApp({ app });
 
-  app.use("/api/users", userRoutes);
-  app.use("/api/order", orderRoutes);
-  app.use("/api/rider", riderRoutes);
-  app.use("/api/tx", riderRoutes);
+//   app.use("/api/users", userRoutes);
+//   app.use("/api/order", orderRoutes);
+//   app.use("/api/rider", riderRoutes);
+//   app.use("/api/tx", riderRoutes);
 
-  app
-    .listen(port, () =>
-      console.log(`Allroutes Logistics server running on port ${port}`)
-    )
-    .on("error", (error) => {
-      console.log(error.message);
-      process.exit(1);
-    });
-}
+//   app
+//     .listen(port, () =>
+//       console.log(`Allroutes Logistics server running on port ${port}`)
+//     )
+//     .on("error", (error) => {
+//       console.log(error.message);
+//       process.exit(1);
+//     });
+// }
 
-startServer();
+// startServer();
 
